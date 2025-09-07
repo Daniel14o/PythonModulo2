92b369bf207180cb9b104f609a81575e7248f5a1
Daniel Leonardo Caceres Montañez <dalecamon@gmail.com>
2025-09-07 11:27:20 -0500

    Simular guardado y cargado de archivos por consola

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,26 +1,23 @@
 def consola(comando: str) -> str:
     """
-    Desarrolla un programa que simule un menú de consola usando la estructura match-case. El programa mostrará una lista de comandos
-     disponibles ("guardar", "cargar", "salir") y el usuario ingresará uno
+    Simula la ejecución de un comando de consola.
 
     Comandos válidos:
-    - "Imprimir": retorna "Guardando archivo..."
-    - "Si el comando no es valido" retorna "Mostrar mensaje de error"..."
+    - "guardar": retorna "Guardando archivo..."
+    - "cargar": retorna "Cargando archivo..."
     - "salir": retorna "Saliendo del programa..."
 
-    Parametros:
+    Parámetros:
         comando (str): texto del comando.
 
-	Conceptos aplicados: Bucle while, match-case, input(), lower().
-
     Retorna:
-        str: Mensaje de resultado de la acción.
+        str: mensaje de resultado de la acción.
     """
     match comando.lower():
-        case "Imprimir":
-            return "Imprimiendo archivo..."
-        case "Mostrar":
-            return "Mostrando archivo..."
+        case "Guardar":
+            return "Guardando archivo..."
+        case "Cargar":
+            return "Cargando archivo..."
         case "Salir":
             return "Saliendo del programa..."
         case _:
@@ -29,8 +26,8 @@ def consola(comando: str) -> str:
 
 if __name__ == "__main__":
     while True:
-        mensaje = input("Ingrese un comando (Imprimir/Mostrar/Salir): ").strip().lower().replace(" ","")
+        mensaje = input("Ingrese un comando (Guardar/Cargar/Salir): ").strip().lower().replace(" ","")
         resultado = consola(mensaje)
         print(resultado)
-        if mensaje == "salir":
+        if mensaje == "Salir":
             break
